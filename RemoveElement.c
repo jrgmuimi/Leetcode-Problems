1f4f697d4e2b749a87f741dae8cb3f94c373aaa6
jrgmuimi <61841422+jrgmuimi@users.noreply.github.com>
2025-02-02 01:08:06 -0500

    Update to Use Two Pointer Approach

--- RemoveElement.c
+++ RemoveElement.c
@@ -1,30 +1,16 @@
-void swap(int* nums, int index1, int index2) // Swap two elements in the nums array given two indices
-{ // Will be useful to bring the "non-val" occurences to the front as desired
-    int cp = nums[index1];
-    nums[index1] = nums[index2];
-    nums[index2] = cp;
-}
-
-// O(n^2) Time Complexity [Worst Case - If all elements in the array are the target value n(n+1) / 2 Operations], O(1) Space Complexity
 int removeElement(int* nums, int numsSize, int val) 
-{
-    int not_val_ct = 0; // Count number of non-val elements
+{ // O(n) Time Complexity, O(1) Space Complexity
+    int not_val_ct = 0; // Number of non target val numbers
+    int placeIndex = 0; // Store a spot where we can shift the non-value numbers over to
 
-    for (int i = 0; i < numsSize; i++) // Check each element to see if it is the targeted value
+    for (int i = 0; i < numsSize; i++)
     {
-        if (nums[i] == val) // If so,
+        if (nums[i] != val) // If a number is not the target val
         {
-            for (int j = i+1; j < numsSize; j++) // In order to shift the non-target values to the front, we search
-            { // For the first non-target value in the rest of the array at the current index (if it exists)
-                if (nums[j] != val)
-                {
-                    swap(nums, i, j); // We then swap or shift their locations 
-                    not_val_ct++; // Since we found a non-target value, we increment the count
-                    break; // Only looking for first occurence, so break once found
-                }
-            }
+            nums[placeIndex] = nums[i]; // Shift the number over to left/beginning
+            placeIndex++; // Increment index to make space for another non-val num
+            not_val_ct++; // Increment non-val counter
         } 
-        else { not_val_ct++; } // If not, increment non-target value count
     }
 
     return not_val_ct;

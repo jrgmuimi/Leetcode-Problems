a93ee7d15035861f058c18733e476a12f43c25c4
jrgmuimi <61841422+jrgmuimi@users.noreply.github.com>
2025-02-03 04:22:59 -0500

    Removed Unnecessary Counter In RemoveElement.c

--- RemoveElement.c
+++ RemoveElement.c
@@ -1,6 +1,5 @@
 int removeElement(int* nums, int numsSize, int val) 
 { // O(n) Time Complexity, O(1) Space Complexity
-    int not_val_ct = 0; // Number of non target val numbers
     int placeIndex = 0; // Store a spot where we can shift the non-value numbers over to
 
     for (int i = 0; i < numsSize; i++)
@@ -9,9 +8,8 @@ int removeElement(int* nums, int numsSize, int val)
         {
             nums[placeIndex] = nums[i]; // Shift the number over to left/beginning
             placeIndex++; // Increment index to make space for another non-val num
-            not_val_ct++; // Increment non-val counter
         } 
     }
 
-    return not_val_ct;
+    return placeIndex;
 }

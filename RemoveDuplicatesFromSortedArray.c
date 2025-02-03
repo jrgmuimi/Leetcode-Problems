63cc3d1e17bbb004b40867c32c0264c83cc03e5e
jrgmuimi <61841422+jrgmuimi@users.noreply.github.com>
2025-02-03 04:27:43 -0500

    Removed Unnecessary Counter In RemoveDuplicatesFromSortedArray.c

--- RemoveDuplicatesFromSortedArray.c
+++ RemoveDuplicatesFromSortedArray.c
@@ -1,19 +1,17 @@
 int removeDuplicates(int* nums, int numsSize) 
 { // O(n) Time Complexity, O(1) Space Complexity
     int lastFound = nums[0]; // Store the last UNIQUE found number
-    int unique = 1; // If array is only one element, start from 1
-    int placeIndex = 1;
+    int placeIndex = 1; // If array is only one element, start from 1
 
     for (int i = 1; i < numsSize; i++)
     {
         if(nums[i] != lastFound) // As we're iterating, if we find a new unique value
         {
-            unique++; // Increment unique counter
             lastFound = nums[i]; // Change last unique found number to new one
             nums[placeIndex] = nums[i]; // Shift the new unique number left by using index pointer
             placeIndex++; // Increment index pointer to prepare for next unique number
         }
     }
 
-    return unique;
+    return placeIndex;
 }
